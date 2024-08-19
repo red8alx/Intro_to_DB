bc78bbd4c4e2e46096a89eb4b732671f8c1d348f
Rediet <red8alx@gmail.com>
2024-08-19 12:05:07 +0300

    Intro to DB

--- task_4.sql
+++ task_4.sql
@@ -1,6 +1,3 @@
 
-SELECT COLUMN_NAME
-FROM 
-    INFORMATION_SCHEMA.COLUMNS
-WHERE 
-    TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+USE `alx_book_store`;
+SHOW FULL COLUMNS FROM `books`;

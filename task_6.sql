97bf5b080f23e5a31493a089c1320ff90833b738
Rediet <red8alx@gmail.com>
2024-08-19 18:32:28 +0300

    Intro to DB

---
+++ task_6.sql
@@ -0,0 +1,5 @@
+USE alx_book_store;
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (2, "Blessing Malik", "cbaidoo@sandtech.com", "123 Happiness Ave."),
+(3, "Obed Ehoneah", "eobed@sandtech.com", "125 Happiness  Ave."),
+(4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness  Ave.");
